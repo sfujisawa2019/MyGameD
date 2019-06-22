52f7d0d410af8024ca16e22149343b2bd97b2136
fujisawa <sfujisawa2019@gmail.com>
2019-06-22 14:05:37 +0900

    練習１前状態

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -105,15 +105,16 @@ bool HelloWorld::init()
 	this->addChild(sprite);
 	sprite->setPosition(Vec2(visibleSize.width/2.0f, visibleSize.height/2.0f));
 
-	//MoveBy* action1 = MoveBy::create(1.0f, Vec2(200, 100));
+	MoveBy* action1 = MoveBy::create(1.0f, Vec2(300, 200));
+	//EaseInOut* action2 = EaseInOut::create(action1, 2.0f);
 	//MoveTo* action1 = MoveTo::create(1.0f, Vec2(200, 100));
 	//ScaleTo* action1 = ScaleTo::create(1.0f, 5.0f);
 	//JumpTo* action1 = JumpTo::create(1.0f, Vec2(200, 100), 300.0f, 2);
-	ccBezierConfig conf;
-	conf.controlPoint_1 = Vec2(200, 200);
-	conf.controlPoint_2 = Vec2(500, 500);
-	conf.endPosition = Vec2(800, 200);
-	BezierTo* action1 = BezierTo::create(2.0f, conf);
+	//ccBezierConfig conf;
+	//conf.controlPoint_1 = Vec2(200, 200);
+	//conf.controlPoint_2 = Vec2(500, 500);
+	//conf.endPosition = Vec2(800, 200);
+	//BezierTo* action1 = BezierTo::create(2.0f, conf);
 	sprite->runAction(action1);
 
 	
