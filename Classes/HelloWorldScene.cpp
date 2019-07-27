3b476395479d0f934e16d6980dc622e29f842db8
fujisawa <sfujisawa2019@gmail.com>
2019-07-27 14:16:01 +0900

    初期状態

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -54,75 +54,7 @@ bool HelloWorld::init()
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-
-    if (closeItem == nullptr ||
-        closeItem->getContentSize().width <= 0 ||
-        closeItem->getContentSize().height <= 0)
-    {
-        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
-    }
-    else
-    {
-        float x = origin.x + visibleSize.width - closeItem->getContentSize().width/2;
-        float y = origin.y + closeItem->getContentSize().height/2;
-        closeItem->setPosition(Vec2(x,y));
-    }
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr)
-    {
-        problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else
-    {
-        // position the label on the center of the screen
-        label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - label->getContentSize().height));
-
-        // add the label as a child to this layer
-        this->addChild(label, 1);
-    }
-
-	// 指定秒数待機するアクション
-	DelayTime* delay = DelayTime::create(1.0f);
-	// 関数を呼び出すアクション
-	//CallFunc* callFunc = CallFunc::create(
-	//	CC_CALLBACK_0(HelloWorld::myFunction, this));
-	//CallFunc* callFunc = CallFunc::create(
-	//	CC_CALLBACK_0(HelloWorld::myFunction2, this, "inu.png"));
-	CallFunc* callFunc = CallFunc::create(
-		CC_CALLBACK_0(HelloWorld::myFunction3, this));
-	// 連続実行アクション
-	Sequence* seq = Sequence::create(delay, callFunc, nullptr);
-
-	this->runAction(seq);
-
-	//this->myFunction();
-	// サウンド再生
-	audioID = experimental::AudioEngine::play2d("testbgm.mp3", true, 0.5f);
-
-	// update関数を有効にする
-	this->scheduleUpdate();
 
     return true;
 }
